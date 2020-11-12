63697f2c4e73c448cffb4b3e73f8286564aa38f8
Sorakime <iztob35690@yahoo.co.jp>
2020-11-12 22:35:46 +0900

    Update license.js

--- mncr/license.js
+++ mncr/license.js
@@ -1,4 +1,5 @@
-if (location.search) {
+const loc=Math(location.href);
+if (loc<1.1) {
   const win=location.search.split('=').slice(-1)[0];
   document.getElementsByTagName('view-1')[0].innerHTML='v.'+win;
   document.getElementsByTagName('title')[0].innerHTML='monochrome. License'+win+' - monochrome Project. - SmncrS';
