125087b6e6c3cb6c33722dda7c2c37b1a570f63b
Sorakime <69241694+Sorakime@users.noreply.github.com>
2021-01-06 15:42:29 +0900

    疲れました。
    podcast....

--- me/music/script/slider.js
+++ me/music/script/slider.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded',()=>{
 });
 
 function moveslider() {
-  document.querySelector('slide-wrapper p:not([data-hide-slider])')[0].setAttribute('data-temp-slider');
-  document.querySelector('slide-wrapper p[data-hide-slider]')[0].removeAttribute('data-hide-slider');
-  document.querySelector('slide-wrapper p[data-temp-slider]')[0].setAttribute('data-hide-slider');
-  document.querySelector('slide-wrapper p[data-temp-slider]')[0].removeAttribute('data-temp-slider');
+  document.querySelector('slide-wrapper p[data-hide-slider]').setAttribute('data-temp-slider',''); //+data-hide-slieder + data-temp-slider
+  document.querySelector('slide-wrapper p[data-temp-slider]').removeAttribute('data-hide-slider'); //+data-temp-slider
+  document.querySelector('slide-wrapper p:not([data-temp-slider])').setAttribute('data-hide-slider',''); //~data-hide-slider
+  document.querySelector('slide-wrapper p[data-temp-slider]').removeAttribute('data-temp-slider'); //
 }
---
+++ me/podcast/script/nav.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName('button-episode')[0].addEventListener('click', ()=>{location.href='program/'})
+document.getElementsByTagName('button-season')[0].addEventListener('click', ()=>{location.href='program/'})
+document.getElementsByTagName('button-program')[0].addEventListener('click', ()=>{location.href='program/'})
+document.getElementsByTagName('album-art')[0].addEventListener('click', ()=>{location.href='program/01/'})
