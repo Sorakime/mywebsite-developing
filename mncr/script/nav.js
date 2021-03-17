3139872ce8b17a5c7c42f9dd2000cc619e39531c
Sorakime <69241694+Sorakime@users.noreply.github.com>
2021-03-17 22:00:35 +0900

    めちゃくちゃ変えたよ！
    うん。
    以上

---
+++ mncr/script/nav.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName('nav-btn')[0].setAttribute('onclick','navopen();');
+function navopen() { //open navigation
+  var navcts=document.getElementsByTagName('nav-cts')[0];
+  document.getElementsByTagName('nav-btn')[0].setAttribute('onclick','navclose();');
+  navcts.setAttribute('id','navbtn');
+}
+function navclose() { //close navigation
+  var navcts=document.getElementsByTagName('nav-cts')[0];
+  navcts.removeAttribute('id');
+  document.getElementsByTagName('nav-btn')[0].setAttribute('onclick','navopen();');
+}
