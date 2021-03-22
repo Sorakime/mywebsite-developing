e069c80d094ce53dd52676a0f59aff9765831ca1
Sorakime <69241694+Sorakime@users.noreply.github.com>
2021-03-22 21:16:40 +0900

    Update license.js

--- mncr/license.js
+++ mncr/license.js
@@ -1,4 +1,4 @@
 const win=location.search.split('=').slice(-1)[0];
 const view=' v.'+win;
 document.getElementsByTagName('view-1')[0].innerHTML=view;
-document.getElementsByTagName('title')[0].innerHTML='monochrome. License'+view+' - monochrome Project. - SmncrS';
+document.getElementsByTagName('title')[0].innerHTML=`monochrome. License ${view} - monochrome Project. - SmncrS`;
