60ba811024eef7be6af705ffb03e91356c7eecfb
Sorakime <69241694+Sorakime@users.noreply.github.com>
2021-07-17 18:05:59 +0900

    bug fixed
    I fixed bug.
    The summary:
    - Fixed the navigation-bar in the vash's page.
    - Changed <h1> tag's font-size when looking at portrait display
    - Fixed the character's color when dark-mode.
    - Fixed The download in the monoJS's webpage.
    
    and more.

--- mncr/script/preload.js
+++ mncr/script/preload.js
@@ -1,4 +1,7 @@
 window.onload=()=>{
   document.getElementsByTagName('body')[0].innerHTML=`${document.getElementsByTagName('body')[0].innerHTML}
   <small>Copyright 2021 Sorakime. Licensed by monochrome. License.</small>`;
+  document.getElementsByTagName('span')[0].addEventListener('click',()=>{
+    location.href='/mncr/';
+  })
 }
