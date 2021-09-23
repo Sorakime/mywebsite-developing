f9021df2d1a87771575766cdf6bce1355b0df55c
Sorakime <69241694+Sorakime@users.noreply.github.com>
2021-09-23 20:45:20 +0900

    a

--- script/onclick.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('view-top').addEventListener('click', () => {
-  location.href='me/';
-});
-document.querySelector('view-bottom').addEventListener('click', () => {
-  location.href='mncr/';
-});
